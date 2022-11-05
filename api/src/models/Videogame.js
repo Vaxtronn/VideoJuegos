412670342d964856c657e434204d835ef87d266c
Vaxtronn <miguel.villam96@gmail.com>
2022-11-05 11:34:03 -0500

    sequalize model added

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      release_date: {
+        type: DataTypes.DATEONLY,
+      },
+      rating: {
+        type: DataTypes.STRING,
+      },
+      platforms: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
